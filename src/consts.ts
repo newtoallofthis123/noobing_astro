b72fc8afdfc2528f0161cf5ce4043f0df02b05ff
Noob Science <noobscience123@gmail.com>
2023-04-06 22:46:49 +0530

    Improve Site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Noobing With The Noob';
+export const SITE_DESCRIPTION = 'Welcome to Noobing with the Noob, where I write my opinions on tech, games, and life.';
